b54213f24405969a9f5398263136fa6f996ed195
adria <adria@DOOMGUY-PC>
2022-04-08 02:13:39 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/pessoasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using crud.Models;
+
+namespace crud.Controllers
+{
+    public class pessoasController : Controller
+    {
+        private contexto db = new contexto();
+
+        // GET: pessoas
+        public ActionResult Index()
+        {
+            return View(db.Pessoas.ToList());
+        }
+
+        // GET: pessoas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pessoa pessoa = db.Pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // GET: pessoas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: pessoas/Create
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Codigo,Nome,Email")] pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pessoas.Add(pessoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pessoa);
+        }
+
+        // GET: pessoas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pessoa pessoa = db.Pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: pessoas/Edit/5
+        // Para se proteger de mais ataques, habilite as propriedades específicas às quais você quer se associar. Para 
+        // obter mais detalhes, veja https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Codigo,Nome,Email")] pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pessoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pessoa);
+        }
+
+        // GET: pessoas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pessoa pessoa = db.Pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: pessoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            pessoa pessoa = db.Pessoas.Find(id);
+            db.Pessoas.Remove(pessoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace crud.Models
+{
+    public class contexto: DbContext
+    {
+            public DbSet<pessoa> Pessoas { get; set; }
+    }
+}
